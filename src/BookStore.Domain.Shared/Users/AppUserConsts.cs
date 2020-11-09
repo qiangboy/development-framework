f8c1c2b53bfcf7e5c82d7c155c0ef369a01adfa0
qiang <2314862535@qq.cm>
2020-11-09 10:33:49 +0800

    添加项目文件。

---
+++ src/BookStore.Domain.Shared/Users/AppUserConsts.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStore.Users
+{
+    public static class AppUserConsts
+    {
+        public const int MaxAvatarLength = 200;
+        public const int MaxIntroductionLength = 1000;
+    }
+}
---
+++ src/BookStore.EntityFrameworkCore.DbMigrations/Migrations/20201104041038_Added_AppUser_Properties.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore.Migrations
+{
+    public partial class Added_AppUser_Properties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "AbpUsers",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Introduction",
+                table: "AbpUsers",
+                maxLength: 1000,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "AbpUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Introduction",
+                table: "AbpUsers");
+        }
+    }
+}
